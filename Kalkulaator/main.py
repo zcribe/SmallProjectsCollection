99a6c2f4d946576ab7604c61e104c9b74f61c055
Erlend Eelmets <f146b3d8@blurmail.net>
2018-08-05 07:27:18 +0300

    Initial commit. UI done.

---
+++ Kalkulaator/main.py
@@ -0,0 +1,15 @@
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+
+
+class CalculatorApp(App):
+    pass
+
+
+class CalculatorRoot(BoxLayout):
+    pass
+
+
+if __name__ == '__main__':
+    CalculatorApp().run()
