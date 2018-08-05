b9b4037dcf8f0f0ee6b77d2c3500a36111f30b6c
Erlend Eelmets <f146b3d8@blurmail.net>
2018-08-05 10:42:24 +0300

    Calculator finished. Gif of in-action.

--- Kalkulaator/main.py
+++ Kalkulaator/main.py
@@ -1,14 +1,21 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.gridlayout import GridLayout
-
-
-class CalculatorApp(App):
-    pass
+from kivy.properties import StringProperty
 
 
 class CalculatorRoot(BoxLayout):
-    pass
+    expression = StringProperty()
+
+    def _evaluate(self, expression):
+        try:
+            self.expression = str(eval(expression))
+        except:
+            self.expression = "Error"
+
+
+class CalculatorApp(App):
+    title = "Kalkulaator"
+    icon = "kalkulaator.png"
 
 
 if __name__ == '__main__':
