761ccf5e74016976d0b2a4ca4938936b52b55c78
Erlend Eelmets <f146b3d8@blurmail.net>
2018-08-07 19:41:25 +0300

    Countdown changes

--- CountdownSolver/main.py
+++ CountdownSolver/main.py
@@ -1,68 +1,16 @@
-from itertools import combinations, permutations, product
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from helpers import countdown_solver
+from kivy.properties import ListProperty, NumericProperty
 
-def reverse_polish(expression):
-    ops = {
-        "+": (lambda a, b: a + b),
-        "-": (lambda a, b: a - b),
-        "*": (lambda a, b: a * b),
-        "/": (lambda a, b: a / b)
-    }
-    tokens = expression.split()
-    stack = []
-
-    for token in tokens:
-        if token in ops:
-            arg2 = stack.pop()
-            arg1 = stack.pop()
-            result = ops[token](arg1, arg2)
-            stack.append(result)
-        else:
-            stack.append(int(token))
-
-    return stack.pop()
+class CountdownRoot(BoxLayout):
+    pass
 
 
-def countdown_solver(goal, nr_list):
-    # Works but re-uses values (Cheats)
-    nr_combinations = combinations(nr_list, 2)
-    permutat = []
-    for i in nr_combinations:
-        permutat.append("+".join(i))
-        permutat.append("-".join(i))
-        permutat.append("*".join(i))
-        permutat.append("/".join(i))
-    perm_comb = combinations(permutat, 3)
-    overall = []
-    for trio in perm_comb:
-        overall.append("+".join(trio))
-        overall.append("-".join(trio))
-        overall.append("*".join(trio))
-        overall.append("/".join(trio))
-    for i in overall:
-        if eval(i) == goal:
-            print("{} = {}".format(i, eval(i)))
-
-
-def countdown_solver_ii(goal, nr_list, length):
-    # Doesn't work but not sure quite why
-    operators = ['+', '-', '*', '/']
-    nr_combs = permutations(nr_list, length)
-    op_combs = product(operators, repeat=length-1)
-    for nr, op in zip(nr_combs, op_combs):
-        result = [None] * (len(nr) + len(op))
-        result[::2] = nr
-        result[1::2] = op
-        stri = ''.join(result)
-        answer = eval(stri)
-        #if int(answer) == int(goal):
-        print('{} = {}'.format(stri, answer))
-
-def length_compensate(goal, nr_list):
-    i = 6
-    while i > 1:
-        countdown_solver_ii(goal, nr_list, i)
-        i -= 1
+class CountdownSolverApp(App):
+    title = 'Countdown solver'
+    icon = 'timer.png'
 
 
 if __name__ == '__main__':
-    countdown_solver(609, ['50', '75', '4', '4', '1', '3'])
+    CountdownSolverApp().run()
