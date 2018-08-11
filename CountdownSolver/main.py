6ff8e98dc595a692ece5bfacd4a86f71ba7c4a63
Erlend Eelmets <f146b3d8@blurmail.net>
2018-08-11 08:22:08 +0300

    Countdown, ModConverter Initial

--- CountdownSolver/main.py
+++ CountdownSolver/main.py
@@ -3,7 +3,7 @@ from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from helpers import countdown_solver
 from kivy.properties import StringProperty, ListProperty
-from kivy.uix.recycleview import RecycleView
+from kivy.uix.recyclelayout import RecycleLayout
 
 
 class CountdownRoot(BoxLayout):
@@ -14,6 +14,7 @@ class CountdownRoot(BoxLayout):
     def results(self):
         self.result = countdown_solver(int(self.goal), self.numbers.split(","))
         print("Solved")
+        print(self.ids.list_view.data)
 
 
 
