be32c0279ab5ce9eccf43138a29074431dd2518d
Erlend Eelmets <debcf78e@opayq.com>
2018-08-12 21:43:14 +0300

    Refactor

--- ModConverter/main.py
+++ ModConverter/main.py
@@ -2,6 +2,10 @@ import os
 import zipfile
 import shutil
 
+mod_folder = "F:\\Steam\\steamapps\\workshop\\content\\281990"
+descriptor_folder = "C:\\Users\\Erlend\\Documents\\Paradox Interactive\\Stellaris\\mod"
+
+
 class cd:
     """Context manager for changing the current working directory"""
     def __init__(self, newPath):
@@ -15,49 +19,48 @@ class cd:
         os.chdir(self.savedPath)
 
 
-mod_folder = "F:\\Steam\\steamapps\\workshop\\content\\281990"
-descriptor_folder = "C:\\Users\\Erlend\\Documents\\Paradox Interactive\\Stellaris\\mod"
+def clean_descriptor_dir(descriptor_folder):
+    with cd(descriptor_folder):
+        all_files = os.listdir()
+        for descriptor in all_files:
+            os.remove(descriptor)
 
-# TODO: Muuda funktsiooniks
-with cd("C:\\Users\\Erlend\\Documents\\Paradox Interactive\\Stellaris\\mod"):
-    files = os.listdir()
-    for i in files:
-        os.remove(i)
 
-with cd(mod_folder): # TODO: Halb stack cd kasutus
-    mods = os.listdir()
-    for folder in mods:
-        specific_mod_dir = "\\".join([mod_folder, folder])
-        with cd(specific_mod_dir):
-            current_files = os.listdir() # TODO: Puruneb kui mingi muu fail on kohvris
-            new_filename = ""
-            try:
-                new_filename = "".join([folder, ".mod"])
-                with zipfile.ZipFile(current_files[0], "r")as myzip:
-                        myzip.extract("descriptor.mod")
+def create_descriptors(mod_folder):
+    with cd(mod_folder): # TODO: Halb stack cd kasutus
+        mods = os.listdir()
+        for folder in mods:
+            specific_mod_dir = "\\".join([mod_folder, folder])
+            with cd(specific_mod_dir):
+                current_files = os.listdir() # TODO: Puruneb kui mingi muu fail on kohvris
+                new_filename = ""
+                try:
+                    new_filename = "".join([folder, ".mod"])
+                    with zipfile.ZipFile(current_files[0], "r")as myzip:
+                            myzip.extract("descriptor.mod")
 
-                mod_path = "\\".join(["workshop\\content\\281990\\LinkedDir", folder, current_files[0]]).replace("\\", "/")
+                    mod_path = "\\".join(["workshop\\content\\281990\\LinkedDir", folder, current_files[0]]).replace("\\", "/")
 
-                f = open("descriptor.mod", "r+", encoding="utf8")
-                d = f.readlines()
-                f.seek(0)
-                for i in d:
-                    if i == 'archive="{}"\n'.format(current_files[0]):
-                        f.write('path="{}"\n'.format(mod_path))
-                    else:
-                        f.write(i)
-                f.truncate()
-                f.close()
+                    f = open("descriptor.mod", "r+", encoding="utf8")
+                    d = f.readlines()
+                    f.seek(0)
+                    for i in d:
+                        if i == 'archive="{}"\n'.format(current_files[0]):
+                            f.write('path="{}"\n'.format(mod_path))
+                        else:
+                            f.write(i)
+                    f.truncate()
+                    f.close()
 
-                os.rename("descriptor.mod", new_filename)
-                shutil.move("\\".join([specific_mod_dir, new_filename]), descriptor_folder )
-            except IndexError:
-                pass
-            except zipfile.BadZipFile:
-                os.remove(new_filename)
-                print("Multiple files in the folder or not a zip file {}".format(folder))
-            except shutil.Error:
-                with cd(descriptor_folder):
-                    files = os.listdir()
-                    for i in files:
-                        os.remove(i)
+                    os.rename("descriptor.mod", new_filename)
+                    shutil.move("\\".join([specific_mod_dir, new_filename]), descriptor_folder )
+                except IndexError:
+                    pass
+                except zipfile.BadZipFile:
+                    os.remove(new_filename)
+                    print("Multiple files in the folder or not a zip file {}".format(folder))
+                except shutil.Error:
+                    with cd(descriptor_folder):
+                        files = os.listdir()
+                        for i in files:
+                            os.remove(i)
