b5f5169dceccf097f5d09d16c8f38557e02b6965
Erlend Eelmets <debcf78e@opayq.com>
2018-09-02 15:23:55 +0300

    Basic definitions

--- VMTranslator/vmTranslator.py
+++ VMTranslator/vmTranslator.py
@@ -9,12 +9,13 @@ import sys
 
 class Parser:
     def __init__(self):
-        self.filename = sys.argv[1].split('.')[0]
+        self.filename = None
         self.document = None
         self.instructions = None
 
     def main(self):
         self.input()
+        self.filename = sys.argv[1].split('.')[0]
         self.whitespace_remover()
         self.instruction_separator()
         return self.instructions, self.filename
@@ -54,55 +55,81 @@ class CodeWriter:
         self.filename = self.parse_result[1]
         for instruction in self.instructions:
             self.instruction_type_separator(instruction)
+        self.output()
 
     def output(self):
-        with open('{}.asm'.format(self.filename), 'w') as f:
+        with open('{}.asm'.format(self.filename), 'w+') as f:
             f.write('\n'.join(self.instructions_assembly))
 
     def instruction_type_separator(self, instruction):
         arg1 = instruction[0]
         if arg1 == 'pop' or arg1 == 'push':
             self.memory_translator(instruction)
-        elif arg1 == 'add' or arg1 == 'sub' or arg1 == 'neg':
+        elif arg1 == 'add' or arg1 == 'sub' or arg1 == 'neg' or arg1 == 'and' or arg1 == 'or' or arg1 == 'not':
             self.arithmetic_translator(instruction)
-        elif arg1 == 'eq' or arg1 == 'gt' or arg1 == 'lt' or arg1 == 'and' or arg1 == 'or' or arg1 == 'not':
+        elif arg1 == 'eq' or arg1 == 'gt' or arg1 == 'lt':
             self.logical_translator(instruction)
-
-    def memory_translator(self, instruction):
-        arg0, arg1, arg2 = instruction
-        if arg1 == 'pop':
-            self.instructions_assembly.extend(None)
         else:
-            # Push
-            symbols = ['// {} {} {}', '@{}', 'D=A', '@SP', 'A=M', 'M=D', '@SP', 'M=M+1']
-            symbols[0] = symbols[0].format(arg0, arg1, arg2)
-            symbols[1] = symbols[1].format(arg2)
-            self.instructions_assembly.extend(symbols)
+            Warning('Unable to seperate instruction -({})'.format(instruction))
+
+    def instruction_translator(self, instruction, template, arg_count=1):
+        symbols = template
+        if arg_count == 3:
+            symbols[0] = symbols[0].format(instruction[0], instruction[1], instruction[2])
+            symbols[1] = symbols[1].format(instruction[2])
+        elif arg_count == 1:
+            symbols[0] = symbols[0].format(instruction[0])
+        else:
+            Warning('Wrong argument count -({})'.format(instruction))
+        self.instructions_assembly.extend(symbols)
 
     def arithmetic_translator(self, instruction):
-        if instruction[0] == 'add':
-            self.instructions_assembly.extend(None)
-        elif instruction[0] == 'sub':
-            self.instructions_assembly.extend(None)
+        arg1 = instruction[0]
+        if arg1 == 'add':
+            template = ['// {}', 'M=M+D']
+            self.instruction_translator(instruction=instruction, template=template)
+        elif arg1 == 'sub':
+            template = ['// {}', 'M=M-D']
+            self.instruction_translator(instruction=instruction, template=template)
+        elif arg1 == 'and':
+            template = ['// {}', 'M=M&D']
+            self.instruction_translator(instruction=instruction, template=template)
+        elif arg1 == 'or':
+            template = ['// {}', 'M=M|D']
+            self.instruction_translator(instruction=instruction, template=template)
+        elif arg1 == 'not':
+            template = ['// {}', 'M=!M']
+            self.instruction_translator(instruction=instruction, template=template)
         else:
-            self.instructions_assembly.extend(None)
+            Warning('Bad instruction -({})'.format(instruction))
 
     def logical_translator(self, instruction):
         arg1 = instruction[0]
         if arg1 == 'eq':
-            self.instructions_assembly.extend(None)
+            template = ['// {}', 'D=M-D', 'D;JEQ']
+            self.instruction_translator(instruction=instruction, template=template)
         elif arg1 == 'gt':
-            self.instructions_assembly.extend(None)
+            template = ['// {}', 'D=M-D', 'D;JGT']
+            self.instruction_translator(instruction=instruction, template=template)
         elif arg1 == 'lt':
-            self.instructions_assembly.extend(None)
-        elif arg1 == 'and':
-            self.instructions_assembly.extend(None)
-        elif arg1 == 'or':
-            self.instructions_assembly.extend(None)
+            template = ['// {}', 'D=M-D', 'D;JLT']
+            self.instruction_translator(instruction=instruction, template=template)
         else:
-            self.instructions_assembly.extend(None)
+            Warning('Bad instruction -({})'.format(instruction))
+
+    def memory_translator(self, instruction):
+        if instruction[0] == 'pop':
+            template = ['// {} {} {}', '@SP', 'D=M', ]
+            self.instruction_translator(instruction=instruction, template=template, arg_count=3)
+        elif instruction[0] == 'push':
+            # Push
+            template = ['// {} {} {}', '@{}', 'D=A', '@SP', 'A=M', 'M=D', '@SP', 'M=M+1']
+            self.instruction_translator(instruction=instruction, template=template, arg_count=3)
+        else:
+            Warning('Bad instruction -({})'.format(instruction))
+
 
 
 if __name__ == '__main__':
-    a = Parser()
+    a = CodeWriter()
     a.main()
